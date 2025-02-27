60b0a50958b517e7a9c8732f1823c50e8b0eb190
lmarian0 <luismarianorivera.25@mi.unc.edu.ar>
2025-02-27 08:46:58 -0300

    Actualizaciones en index y about

--- nav.js
+++ nav.js
@@ -1,8 +1,19 @@
-let light = false;
+//Guardar preferencia de modo en caso de no haber preferencia almacenada
+if(localStorage.getItem("modo")===null){
+    localStorage.setItem("modo", JSON.stringify(false));
+}
 
+
+//Variable light almacena el valor booleano de la cadena almacenada
+let light = JSON.parse(localStorage.getItem("modo"));
+console.log(light)
 
 function cambioDeTema(){
+    //Cambio de valor de preferencia
     light = !light;
+    localStorage.setItem("modo", JSON.stringify(light));
+    console.log(light);
+    //Cambio de tema
     if(light){
         document.querySelectorAll(".texto").forEach(elemento => {
             elemento.style.color = "#272727";
@@ -20,3 +31,27 @@ function cambioDeTema(){
         document.getElementById("foto").style.boxShadow = "inset 0 0 1vh 0.5vh #272727";
     }
 }
+
+//Funcion para tomar preferencias de usuario preestablecidas
+
+function iniciodetema(){
+    console.log(light);
+    if(light){
+        document.querySelectorAll(".texto").forEach(elemento => {
+            elemento.style.color = "#272727";
+        });
+        document.body.style.backgroundColor = "#EFFFFB";
+        document.getElementById("foto").style.borderColor = "#272727";
+        document.getElementById("foto").style.boxShadow = "inset 0 0 1vh 0.5vh #EFFFFB";
+
+    }else{
+        document.querySelectorAll(".texto").forEach(elemento => {
+            elemento.style.color = "#EFFFFB";
+        });
+        document.body.style.backgroundColor = "#272727";
+        document.getElementById("foto").style.borderColor = "#EFFFFB";
+        document.getElementById("foto").style.boxShadow = "inset 0 0 1vh 0.5vh #272727";
+    }
+}
+
+iniciodetema();
