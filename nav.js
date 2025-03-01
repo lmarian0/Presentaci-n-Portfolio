9b3b7abb355cb51704c84826f713f2f60b7019fa
lmarian0 <luismarianorivera.25@mi.unc.edu.ar>
2025-03-01 08:25:44 -0300

    Creacion de apartado proyectos

--- nav.js
+++ nav.js
@@ -12,45 +12,55 @@ function cambioDeTema(){
     //Cambio de valor de preferencia
     light = !light;
     localStorage.setItem("modo", JSON.stringify(light));
-    console.log(light);
     //Cambio de tema
     if(light){
         document.querySelectorAll(".texto").forEach(elemento => {
             elemento.style.color = "#272727";
         });
         document.body.style.backgroundColor = "#EFFFFB";
-        document.getElementById("foto").style.borderColor = "#272727";
-        document.getElementById("foto").style.boxShadow = "inset 0 0 1vh 0.5vh #EFFFFB";
+        document.querySelectorAll(".fotos").forEach(elemento =>{
+            elemento.style.borderColor = "#272727";
+            elemento.style.boxShadow = "inset 0 0 1vh 0.5vh #EFFFFB";
+        });
+        document.getElementById("caja_git").style.borderColor="#272727";
 
     }else{
         document.querySelectorAll(".texto").forEach(elemento => {
             elemento.style.color = "#EFFFFB";
         });
         document.body.style.backgroundColor = "#272727";
-        document.getElementById("foto").style.borderColor = "#EFFFFB";
-        document.getElementById("foto").style.boxShadow = "inset 0 0 1vh 0.5vh #272727";
+        document.querySelectorAll(".fotos").forEach(elemento =>{
+            elemento.style.borderColor = "#EFFFFB";
+            elemento.style.boxShadow = "inset 0 0 1vh 0.5vh #272727";
+        });
+        document.getElementById("caja_git").style.borderColor="#EFFFFB";
     }
 }
 
 //Funcion para tomar preferencias de usuario preestablecidas
 
 function iniciodetema(){
-    console.log(light);
     if(light){
         document.querySelectorAll(".texto").forEach(elemento => {
             elemento.style.color = "#272727";
         });
         document.body.style.backgroundColor = "#EFFFFB";
-        document.getElementById("foto").style.borderColor = "#272727";
-        document.getElementById("foto").style.boxShadow = "inset 0 0 1vh 0.5vh #EFFFFB";
+        document.querySelectorAll(".fotos").forEach(elemento =>{
+            elemento.style.borderColor = "#272727";
+            elemento.style.boxShadow = "inset 0 0 1vh 0.5vh #EFFFFB";
+        });
+        document.getElementById("caja_git").style.borderColor="#272727";
 
     }else{
         document.querySelectorAll(".texto").forEach(elemento => {
             elemento.style.color = "#EFFFFB";
         });
         document.body.style.backgroundColor = "#272727";
-        document.getElementById("foto").style.borderColor = "#EFFFFB";
-        document.getElementById("foto").style.boxShadow = "inset 0 0 1vh 0.5vh #272727";
+        document.querySelectorAll(".fotos").forEach(elemento =>{
+            elemento.style.borderColor = "#EFFFFB";
+            elemento.style.boxShadow = "inset 0 0 1vh 0.5vh #272727";
+        });
+        document.getElementById("caja_git").style.borderColor="#EFFFFB";
     }
 }
 
